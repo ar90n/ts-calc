4cf5efe297640042556f0e6c9bd4cb641723a20d
Masahiro Wada <argon.argon.argon@gmail.com>
2020-09-02 11:26:53 +0000

    Add parser

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,7 +1,34 @@
 import { ObjectReadableMock, ObjectWritableMock } from 'stream-mock';
-import { DropDelimiterTransform, ParseNaturalNumbreaTransform } from './parser';
-import { ZERO, DELIMITER, MINUS, NON_ZERO_DIGIT } from './lexer.type';
-import { NATURAL_NUMBER } from './parser.type';
+import {
+  DropDelimiterTransform,
+  parseZero,
+  parsePlus,
+  parseMinus,
+  parseMult,
+  parseDiv,
+  parseSign,
+  parseNonZeroDigit,
+  parseDigit,
+  parseNaturalNumber,
+  parseNumber,
+  parseOp0,
+  parseOp1,
+  parseTerm,
+  parseExpr,
+} from './parser';
+import {
+  ZERO,
+  DELIMITER,
+  FUNCTION,
+  PLUS,
+  MINUS,
+  NON_ZERO_DIGIT,
+  DOT,
+  MULT,
+  DIV,
+  LEFT_PAREN,
+  RIGHT_PAREN,
+} from './lexer.type';
 
 test('drop delimiters', done => {
   const input = [ZERO.of(), DELIMITER.of(' '), ZERO.of()];
@@ -16,21 +43,446 @@ test('drop delimiters', done => {
   });
 });
 
-test('parse natural number', done => {
-  const input = [MINUS.of(), NON_ZERO_DIGIT.of('1'), ZERO.of(), ZERO.of(), NON_ZERO_DIGIT.of('8')];
-  const parseNaturalNumber = new ParseNaturalNumbreaTransform({ objectMode: true });
-  const reader = new ObjectReadableMock(input);
-  const writer = new ObjectWritableMock();
-  reader.pipe(parseNaturalNumber).pipe(writer);
+test('parse zero', () => {
+  const input = [ZERO.of()];
+  const ret = parseZero(input);
+  expect(ret).toEqual({ consumedNodes: 1, output: { kind: 'zero', value: '0' }, status: 'ok' });
+});
 
-  writer.on('finish', () => {
-    expect(writer.data).toEqual([
-      NATURAL_NUMBER.of(MINUS.of(), NON_ZERO_DIGIT.of('1'), [
-        ZERO.of(),
-        ZERO.of(),
-        NON_ZERO_DIGIT.of('8'),
-      ]),
-    ]);
-    done();
+test('parse plus', () => {
+  const input = [PLUS.of()];
+  const ret = parsePlus(input);
+  expect(ret).toEqual({ consumedNodes: 1, output: { kind: 'plus', value: '+' }, status: 'ok' });
+});
+
+test('parse minus', () => {
+  const input = [MINUS.of()];
+  const ret = parseMinus(input);
+  expect(ret).toEqual({ consumedNodes: 1, output: { kind: 'minus', value: '-' }, status: 'ok' });
+});
+
+test('parse mult', () => {
+  const input = [MULT.of()];
+  const ret = parseMult(input);
+  expect(ret).toEqual({ consumedNodes: 1, output: { kind: 'mult', value: '*' }, status: 'ok' });
+});
+
+test('parse div', () => {
+  const input = [DIV.of()];
+  const ret = parseDiv(input);
+  expect(ret).toEqual({ consumedNodes: 1, output: { kind: 'div', value: '/' }, status: 'ok' });
+});
+
+test('parse op0', () => {
+  const input = [PLUS.of(), MINUS.of()];
+  const output = [parseOp0(input.slice(0)), parseOp0(input.slice(1))];
+  expect(output[0]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'plus', value: '+' },
+    status: 'ok',
+  });
+  expect(output[1]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'minus', value: '-' },
+    status: 'ok',
+  });
+});
+
+test('parse op1', () => {
+  const input = [MULT.of(), DIV.of()];
+  const output = [parseOp1(input.slice(0)), parseOp1(input.slice(1))];
+  expect(output[0]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'mult', value: '*' },
+    status: 'ok',
+  });
+  expect(output[1]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'div', value: '/' },
+    status: 'ok',
+  });
+});
+
+test('parse non zero digit', () => {
+  const input = [NON_ZERO_DIGIT.of('1')];
+  const ret = parseNonZeroDigit(input);
+  expect(ret).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'non_zero_digit', value: '1' },
+    status: 'ok',
+  });
+});
+
+test('parse digit', () => {
+  const input = [NON_ZERO_DIGIT.of('1'), ZERO.of()];
+  const output = [parseDigit(input.slice(0)), parseDigit(input.slice(1))];
+  expect(output[0]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'non_zero_digit', value: '1' },
+    status: 'ok',
+  });
+  expect(output[1]).toEqual({
+    consumedNodes: 1,
+    output: { kind: 'zero', value: '0' },
+    status: 'ok',
+  });
+});
+
+test('parse sign', () => {
+  const input = [PLUS.of(), MINUS.of()];
+  const output = [parseSign(input.slice(0)), parseSign(input.slice(1))];
+  expect(output[0]).toEqual({
+    consumedNodes: 1,
+    output: {
+      kind: 'plus',
+      value: '+',
+    },
+    status: 'ok',
+  });
+  expect(output[1]).toEqual({
+    consumedNodes: 1,
+    output: {
+      kind: 'minus',
+      value: '-',
+    },
+    status: 'ok',
+  });
+});
+
+test('parse natural number', () => {
+  const input = [MINUS.of(), NON_ZERO_DIGIT.of('1'), ZERO.of(), ZERO.of(), NON_ZERO_DIGIT.of('8')];
+  const ret = parseNaturalNumber(input);
+  expect(ret).toEqual({
+    consumedNodes: 5,
+    output: {
+      kind: 'natural_number',
+      value: [
+        { kind: 'minus', value: '-' },
+        { kind: 'non_zero_digit', value: '1' },
+        [
+          { kind: 'zero', value: '0' },
+          { kind: 'zero', value: '0' },
+          { kind: 'non_zero_digit', value: '8' },
+        ],
+      ],
+    },
+    status: 'ok',
+  });
+});
+
+test('parse number', () => {
+  const input = [
+    MINUS.of(),
+    NON_ZERO_DIGIT.of('1'),
+    ZERO.of(),
+    ZERO.of(),
+    NON_ZERO_DIGIT.of('8'),
+    DOT.of(),
+    ZERO.of(),
+    ZERO.of(),
+    NON_ZERO_DIGIT.of('8'),
+  ];
+
+  const ret = parseNumber(input);
+  expect(ret).toEqual({
+    consumedNodes: 9,
+    output: {
+      kind: 'number',
+      value: [
+        {
+          kind: 'natural_number',
+          value: [
+            { kind: 'minus', value: '-' },
+            { kind: 'non_zero_digit', value: '1' },
+            [
+              { kind: 'zero', value: '0' },
+              { kind: 'zero', value: '0' },
+              { kind: 'non_zero_digit', value: '8' },
+            ],
+          ],
+        },
+        {
+          kind: 'fraction',
+          value: [
+            { kind: 'dot', value: '.' },
+            [
+              { kind: 'zero', value: '0' },
+              { kind: 'zero', value: '0' },
+              { kind: 'non_zero_digit', value: '8' },
+            ],
+          ],
+        },
+      ],
+    },
+    status: 'ok',
+  });
+});
+
+test('parse term', () => {
+  const input = [
+    NON_ZERO_DIGIT.of('3'),
+    NON_ZERO_DIGIT.of('1'),
+    DOT.of(),
+    NON_ZERO_DIGIT.of('1'),
+    ZERO.of(),
+    ZERO.of(),
+    NON_ZERO_DIGIT.of('8'),
+    MULT.of(),
+    ZERO.of(),
+    DOT.of(),
+    NON_ZERO_DIGIT.of('1'),
+  ];
+
+  const ret = parseTerm(input);
+  expect(ret).toEqual({
+    consumedNodes: 11,
+    output: {
+      kind: 'term',
+      value: [
+        {
+          kind: 'number',
+          value: [
+            {
+              kind: 'natural_number',
+              value: [
+                null,
+                { kind: 'non_zero_digit', value: '3' },
+                [{ kind: 'non_zero_digit', value: '1' }],
+              ],
+            },
+            {
+              kind: 'fraction',
+              value: [
+                { kind: 'dot', value: '.' },
+                [
+                  { kind: 'non_zero_digit', value: '1' },
+                  { kind: 'zero', value: '0' },
+                  { kind: 'zero', value: '0' },
+                  { kind: 'non_zero_digit', value: '8' },
+                ],
+              ],
+            },
+          ],
+        },
+        [
+          { kind: 'mult', value: '*' },
+          {
+            kind: 'term',
+            value: [
+              {
+                kind: 'number',
+                value: [
+                  { kind: 'zero', value: '0' },
+                  {
+                    kind: 'fraction',
+                    value: [{ kind: 'dot', value: '.' }, [{ kind: 'non_zero_digit', value: '1' }]],
+                  },
+                ],
+              },
+              null,
+            ],
+          },
+        ],
+      ],
+    },
+    status: 'ok',
+  });
+});
+
+test('parse expr', () => {
+  const input = [
+    NON_ZERO_DIGIT.of('3'),
+    NON_ZERO_DIGIT.of('1'),
+    DOT.of(),
+    NON_ZERO_DIGIT.of('1'),
+    ZERO.of(),
+    ZERO.of(),
+    NON_ZERO_DIGIT.of('8'),
+    PLUS.of(),
+    LEFT_PAREN.of(),
+    NON_ZERO_DIGIT.of('8'),
+    PLUS.of(),
+    ZERO.of(),
+    RIGHT_PAREN.of(),
+    MULT.of(),
+    FUNCTION.of('sin'),
+    LEFT_PAREN.of(),
+    ZERO.of(),
+    DOT.of(),
+    NON_ZERO_DIGIT.of('1'),
+    PLUS.of(),
+    NON_ZERO_DIGIT.of('2'),
+    RIGHT_PAREN.of(),
+  ];
+
+  const ret = parseExpr(input);
+  expect(ret).toEqual({
+    status: 'ok',
+    consumedNodes: 22,
+    output: {
+      kind: 'expr',
+      value: [
+        {
+          kind: 'term',
+          value: [
+            {
+              kind: 'number',
+              value: [
+                {
+                  kind: 'natural_number',
+                  value: [
+                    null,
+                    { kind: 'non_zero_digit', value: '3' },
+                    [{ kind: 'non_zero_digit', value: '1' }],
+                  ],
+                },
+                {
+                  kind: 'fraction',
+                  value: [
+                    { kind: 'dot', value: '.' },
+                    [
+                      { kind: 'non_zero_digit', value: '1' },
+                      { kind: 'zero', value: '0' },
+                      { kind: 'zero', value: '0' },
+                      { kind: 'non_zero_digit', value: '8' },
+                    ],
+                  ],
+                },
+              ],
+            },
+            null,
+          ],
+        },
+        [
+          { kind: 'plus', value: '+' },
+          {
+            kind: 'expr',
+            value: [
+              {
+                kind: 'term',
+                value: [
+                  {
+                    kind: 'call',
+                    value: [
+                      null,
+                      { kind: 'left_paren', value: '(' },
+                      {
+                        kind: 'expr',
+                        value: [
+                          {
+                            kind: 'term',
+                            value: [
+                              {
+                                kind: 'number',
+                                value: [
+                                  {
+                                    kind: 'natural_number',
+                                    value: [null, { kind: 'non_zero_digit', value: '8' }, []],
+                                  },
+                                  null,
+                                ],
+                              },
+                              null,
+                            ],
+                          },
+                          [
+                            { kind: 'plus', value: '+' },
+                            {
+                              kind: 'expr',
+                              value: [
+                                {
+                                  kind: 'term',
+                                  value: [
+                                    { kind: 'number', value: [{ kind: 'zero', value: '0' }, null] },
+                                    null,
+                                  ],
+                                },
+                                null,
+                              ],
+                            },
+                          ],
+                        ],
+                      },
+                      { kind: 'right_paren', value: ')' },
+                    ],
+                  },
+                  [
+                    { kind: 'mult', value: '*' },
+                    {
+                      kind: 'term',
+                      value: [
+                        {
+                          kind: 'call',
+                          value: [
+                            { kind: 'function', value: 'sin' },
+                            { kind: 'left_paren', value: '(' },
+                            {
+                              kind: 'expr',
+                              value: [
+                                {
+                                  kind: 'term',
+                                  value: [
+                                    {
+                                      kind: 'number',
+                                      value: [
+                                        { kind: 'zero', value: '0' },
+                                        {
+                                          kind: 'fraction',
+                                          value: [
+                                            { kind: 'dot', value: '.' },
+                                            [{ kind: 'non_zero_digit', value: '1' }],
+                                          ],
+                                        },
+                                      ],
+                                    },
+                                    null,
+                                  ],
+                                },
+                                [
+                                  { kind: 'plus', value: '+' },
+                                  {
+                                    kind: 'expr',
+                                    value: [
+                                      {
+                                        kind: 'term',
+                                        value: [
+                                          {
+                                            kind: 'number',
+                                            value: [
+                                              {
+                                                kind: 'natural_number',
+                                                value: [
+                                                  null,
+                                                  { kind: 'non_zero_digit', value: '2' },
+                                                  [],
+                                                ],
+                                              },
+                                              null,
+                                            ],
+                                          },
+                                          null,
+                                        ],
+                                      },
+                                      null,
+                                    ],
+                                  },
+                                ],
+                              ],
+                            },
+                            { kind: 'right_paren', value: ')' },
+                          ],
+                        },
+                        null,
+                      ],
+                    },
+                  ],
+                ],
+              },
+              null,
+            ],
+          },
+        ],
+      ],
+    },
   });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -1,35 +1,19 @@
-import { Transform, TransformOptions, TransformCallback } from 'stream';
-
+import { Transform, TransformCallback } from 'stream';
 import {
-  //  ZERO,
+  ZERO,
   NON_ZERO_DIGIT,
-  //  PLUS,
-  //  MINUS,
-  //  MULT,
-  //  DIV,
-  //  DOT,
-  //  FUNCTION,
-  //  LEFT_PAREN,
-  //  RIGHT_PAREN,
+  PLUS,
+  MINUS,
+  MULT,
+  DIV,
+  DOT,
+  FUNCTION,
+  LEFT_PAREN,
+  RIGHT_PAREN,
   DELIMITER,
 } from './lexer.type';
 
-import {
-  SIGN,
-  DIGIT,
-  NATURAL_NUMBER,
-  //INTEGRAL_NUMBER,
-  //FRACTION,
-  //NUMBER,
-  //OP0,
-  //OP1,
-  //RH_EXPR,
-  //EXPR,
-  //RH_TERM,
-  //TERM,
-  //CALL,
-  //FACTOR,
-} from './parser.type';
+import { NATURAL_NUMBER, FRACTION, NUMBER, EXPR, TERM, CALL, NODE } from './parser.type';
 
 export class DropDelimiterTransform extends Transform {
   _transform(chunk: string | Buffer, encoding: string, done: TransformCallback): void {
@@ -41,81 +25,177 @@ export class DropDelimiterTransform extends Transform {
   }
 }
 
-export class ParseExprTransform extends Transform {
-  _transform(chunk: string | Buffer, encoding: string, done: TransformCallback): void {
-    this.push(chunk);
-    done();
-  }
-}
+const parserStatusValues = ['ok', 'failed'];
+type ParserStatus = typeof parserStatusValues[number];
 
-export class ParseFactorTransform extends Transform {
-  _transform(chunk: string | Buffer, encoding: string, done: TransformCallback): void {
-    this.push(chunk);
-    done();
-  }
-}
+type ParserResult = {
+  status: ParserStatus;
+  consumedNodes: number;
+  output: NODE | NODE[] | null;
+};
 
-export class ParseNaturalNumbreaTransform extends Transform {
-  static readonly stateValues = ['sign', 'non_zero_digit', 'digits'] as const;
-  state: typeof ParseNaturalNumbreaTransform.stateValues[number];
-  sign: SIGN | null;
-  non_zero_digit?: NON_ZERO_DIGIT;
-  digits: DIGIT[];
+const ParseFailed: ParserResult = {
+  status: 'failed',
+  consumedNodes: 0,
+  output: null,
+};
 
-  constructor(opt?: TransformOptions) {
-    super(opt);
-
-    this.state = 'sign' as const;
-    this.sign = null;
-    this.non_zero_digit = undefined;
-    this.digits = [];
+const createBasicParser = (p: any) => (v: NODE[]) => {
+  if (0 === v.length) {
+    return ParseFailed;
   }
 
-  _transform(chunk: string | Buffer, encoding: string, done: TransformCallback): void {
-    if (this.state === 'sign') {
-      this.state = 'non_zero_digit';
-      if (SIGN.is(chunk)) {
-        this.sign = chunk;
-        done();
-        return;
+  if (v[0].kind === p.tag) {
+    return {
+      status: 'ok',
+      consumedNodes: 1,
+      output: v[0],
+    };
+  }
+
+  return ParseFailed;
+};
+
+export const parseZero = createBasicParser(ZERO);
+export const parseNonZeroDigit = createBasicParser(NON_ZERO_DIGIT);
+export const parsePlus = createBasicParser(PLUS);
+export const parseMinus = createBasicParser(MINUS);
+export const parseMult = createBasicParser(MULT);
+export const parseDiv = createBasicParser(DIV);
+export const parseDot = createBasicParser(DOT);
+export const parseFunction = createBasicParser(FUNCTION);
+export const parseLeftParen = createBasicParser(LEFT_PAREN);
+export const parseRightParen = createBasicParser(RIGHT_PAREN);
+
+const Optional = (p: any) => {
+  return (v: NODE[]) => {
+    const ret = p(v);
+    if (ret.status === 'ok') {
+      return ret;
+    }
+
+    return {
+      status: 'ok',
+      consumedNodes: 0,
+      output: null,
+    };
+  };
+};
+
+const Repeat = (p: any) => {
+  return (v: NODE[]) => {
+    let consumedNodes = 0;
+    const output = [];
+    /* eslint no-constant-condition: 0 */
+    while (true) {
+      const cur = p(v.slice(consumedNodes));
+      if (cur.status !== 'ok') {
+        return {
+          status: 'ok',
+          consumedNodes: consumedNodes,
+          output: output,
+        };
       }
-      this.sign = null;
-    }
 
-    if (this.state === 'non_zero_digit') {
-      if (NON_ZERO_DIGIT.is(chunk)) {
-        this.state = 'digits';
-        this.non_zero_digit = (chunk as unknown) as NON_ZERO_DIGIT;
-        done();
-        return;
+      consumedNodes += cur.consumedNodes;
+      output.push(cur.output);
+    }
+  };
+};
+
+const Sequence = (ps: any[]) => {
+  return (v: NODE[]) => {
+    const ret = { ...ParseFailed };
+    for (const p of ps) {
+      const cur = p(v.slice(ret.consumedNodes));
+      if (cur.status !== 'ok') {
+        return ParseFailed;
+      }
+
+      ret.status = 'ok';
+      ret.consumedNodes += cur.consumedNodes;
+      if (ret.output === null) {
+        ret.output = [];
+      }
+      if (ret.output instanceof Array) {
+        ret.output.push(cur.output);
       }
     }
 
-    if (this.state === 'digits' && this.non_zero_digit !== undefined) {
-      if (DIGIT.is(chunk)) {
-        this.digits.push(chunk);
-        done();
-        return;
-      } else {
-        this.state = 'sign';
-        this.push(NATURAL_NUMBER.of(this.sign, this.non_zero_digit, this.digits));
-        done();
-        return;
+    return ret;
+  };
+};
+
+const Or = (ps: any[]) => {
+  return (v: NODE[]) => {
+    for (const p of ps) {
+      const ret = p(v);
+      if (ret.status === 'ok') {
+        return ret;
       }
     }
 
-    //raise here
-    done();
-  }
+    return ParseFailed;
+  };
+};
 
-  _flush(done: TransformCallback): void {
-    if (this.state === 'digits' && this.non_zero_digit !== undefined) {
-      this.push(NATURAL_NUMBER.of(this.sign, this.non_zero_digit, this.digits));
-      done();
-      return;
-    }
+const Lazy = (): [any, any] => {
+  let p: any = null;
+  return [
+    (v: NODE[]) => {
+      return p(v);
+    },
+    (q: any) => {
+      p = q;
+    },
+  ];
+};
 
-    //raise here
-    done();
-  }
-}
+const Just = (kind: any, p: any) => {
+  return (v: NODE[]) => {
+    const { status, consumedNodes, output } = p(v);
+    return {
+      status,
+      consumedNodes,
+      output: {
+        kind: kind,
+        value: output,
+      },
+    };
+  };
+};
+
+export const parseSign = Or([parsePlus, parseMinus]);
+export const parseDigit = Or([parseZero, parseNonZeroDigit]);
+export const parseOp0 = parseSign;
+export const parseOp1 = Or([parseMult, parseDiv]);
+export const parseNaturalNumber = Just(
+  NATURAL_NUMBER.tag,
+  Sequence([Optional(parseSign), parseNonZeroDigit, Repeat(parseDigit)]),
+);
+export const parseIntegralNumber = Or([parseNaturalNumber, parseZero]);
+export const parseFraction = Just(FRACTION.tag, Sequence([parseDot, Repeat(parseDigit)]));
+export const parseNumber = Just(
+  NUMBER.tag,
+  Sequence([parseIntegralNumber, Optional(parseFraction)]),
+);
+
+const [_parseExpr, setParseExpr] = Lazy();
+const [_parseTerm, setParseTerm] = Lazy();
+const [_parseFactor, setParseFactor] = Lazy();
+export const parseExpr = Just(
+  EXPR.tag,
+  Sequence([_parseTerm, Optional(Sequence([parseOp0, _parseExpr]))]),
+);
+export const parseTerm = Just(
+  TERM.tag,
+  Sequence([_parseFactor, Optional(Sequence([parseOp1, _parseTerm]))]),
+);
+export const parseFactor = Or([
+  Just(CALL.tag, Sequence([Optional(parseFunction), parseLeftParen, parseExpr, parseRightParen])),
+  parseNumber,
+]);
+
+setParseExpr(parseExpr);
+setParseTerm(parseTerm);
+setParseFactor(parseFactor);
